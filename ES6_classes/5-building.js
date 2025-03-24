8cc4b2563ee4a861b0e52b3527fb2b905e732451
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-24 17:45:59 +0100

    5 maj

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,17 @@
-export default class Building {
+class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') { throw new Error('Class extending Building must override evacuationWarningMessage'); }
-    this.sqft = sqft;
+    this._sqft = sqft;
+
+    // Check if the subclass implements evacuationWarningMessage
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
-  get sqft() {return this._sqft;}
-
-  set sqft(newValue) {this._sqft = newValue;}
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
 }
+
+export default Building;
