4f6cff5e18e2e9064cfdd2ceb10b1ecbcd2cf6fa
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-31 16:57:17 +0200

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
-  return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  const result = [];
+  set.forEach((item) => {
+    if (item.startsWith(startString)) {
+      const restOfString = item.substring(startString.length);
+      result.push(restOfString);
+    }
+  });
+
+  return result.join('-');
 }
