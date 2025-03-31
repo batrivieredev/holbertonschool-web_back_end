8390d01061ec750659b5d90e2507524cc5df0b4d
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-31 16:56:18 +0200

    0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,13 @@
 export default function getListStudents() {
-  return [
+  const listStudents = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return listStudents.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
 }
