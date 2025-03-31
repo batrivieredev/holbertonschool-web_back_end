0a5a5026243a80fc27408956c266faf07735b0ab
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-31 16:56:56 +0200

    `Refactor ES6 data manipulation functions to improve code structure and consistency.`

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
+  return students.filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
+      const SGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return { ...student, grade: SGrade ? SGrade.grade : 'N/A' };
     });
 }
