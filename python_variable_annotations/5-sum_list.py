3289811a201ed93b6d3d7d2b7fb6b93216f03c63
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 16:01:32 +0200

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""Module avec annotations de type qui fait la somme d'une liste de nombres flottants."""
-
+"""Returns the sum of a list of floats."""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """Fait la somme d'une liste de nombres à virgule flottante et retourne le résultat."""
+    """Returns the sum of a list of floats."""
     return sum(input_list)
-
