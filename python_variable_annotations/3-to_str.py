49ccb110527d4885f929de766b8f9a565a324846
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 15:54:54 +0200

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module avec annotations de type
+qui prend un float n en argument
+et retourne la représentation en chaîne de caractères de ce float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Retourne la représentation en chaîne de caractères du nombre à virgule flottante."""
+    return str(n)
+
