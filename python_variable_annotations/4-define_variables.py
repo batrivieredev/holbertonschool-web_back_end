69168661cdbf7984470add11c7da6026c1026c56
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 16:01:27 +0200

    4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-"""Module qui définit des variables avec des annotations de type."""
-
-a: int = 1  # Variable entière
-pi: float = 3.14  # Nombre à virgule flottante
-i_understand_annotations: bool = True  # Booléen
-school: str = "Holberton"  # Chaîne de caractères
-
+"""Variable annotations and definitions"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
