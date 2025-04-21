047220f8fb4813d8d494194fd0b885436ab51a10
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 16:01:42 +0200

    7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""
-Module avec annotations de type qui prend
-une chaîne de caractères k et un entier ou nombre flottant v comme arguments
-et retourne un tuple.
-"""
-
+"""Returns a tuple with the string k and the square of int/float v."""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Retourne un tuple contenant la chaîne et le carré du nombre."""
-    return (k, v * v)
-
+    """Returns a tuple with the string k and the square of int/float v."""
+    return (k, v ** 2.0)
