46edabfd541aa73b220b115064427182ce469141
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 15:54:36 +0200

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Fonction annotée avec des types : add
+Elle prend deux arguments de type float (a et b)
+et retourne leur somme en tant que float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Additionne deux nombres à virgule flottante et retourne le résultat."""
+    return (a + b)
+
