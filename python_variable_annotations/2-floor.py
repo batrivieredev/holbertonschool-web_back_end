0deeda39f331b3a2cd1c84228fc3c23ef3f7e98b
batrivieredev <baptiste.rivierefr@gmail.com>
2025-04-21 15:54:49 +0200

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module avec annotations de type : floor
+Prend un float n en argument et retourne la partie entière (plancher) de ce float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Retourne la partie entière (plancher) du nombre à virgule flottante."""
+    return math.floor(n)
+
