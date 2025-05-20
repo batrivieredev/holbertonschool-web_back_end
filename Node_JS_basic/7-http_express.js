3dc4a49b5dd5a877f22be0788f6b8851d86251ab
batrivieredev <baptiste.rivierefr@gmail.com>
2025-05-20 09:23:57 +0200

    7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,26 +2,20 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const args = process.argv.slice(2);
+const database = args[0];
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    if (!data) {
-      throw new Error('Cannot load the database');
-    }
-
     const lines = data.toString().split('\n');
-    const students = lines.slice(1).filter(line => line.length > 0);
+    const students = lines.slice(1).filter((line) => line.length > 0);
+    let message = '';
 
-    if (students.length === 0) {
-      throw new Error('Cannot load the database');
-    }
+    message += `Number of students: ${students.length}\n`;
 
-    let output = `Number of students: ${students.length}\n`;
-
-    // Group students by field
     const fields = {};
-    students.forEach(student => {
+    students.forEach((student) => {
       const [firstName, , , field] = student.split(',');
       if (!fields[field]) {
         fields[field] = [];
@@ -29,25 +23,24 @@ function countStudents(path) {
       fields[field].push(firstName);
     });
 
-    // Add results for each field (sorted alphabetically)
-    Object.keys(fields).sort().forEach(field => {
-      output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-    });
+    for (const [field, names] of Object.entries(fields).sort()) {
+      message += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
 
-    return output;
+    return message;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
+};
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
+app.get('/students', (_, res) => {
   try {
-    const output = countStudents(process.argv[2]);
-    res.send(`This is the list of our students\n${output}`);
+    const message = countStudents(database);
+    res.send(`This is the list of our students\n${message.slice(0, -1)}`);
   } catch (error) {
     res.send(`This is the list of our students\n${error.message}`);
   }
