45adfe11810e6b780ed760dde581537068d82b2f
batrivieredev <baptiste.rivierefr@gmail.com>
2025-05-20 09:10:27 +0200

    6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,8 +4,7 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+  res.status(200).type('text/plain').send('Hello Holberton School!');
 });
 
 app.listen(port);
